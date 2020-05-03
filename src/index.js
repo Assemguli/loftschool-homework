d64382f1a954e96b0b042b7ec69739f278665f60
Assemguli <kassemguli@gmail.com>
2020-05-03 15:28:09 +0600

    added homework three

--- src/index.js
+++ src/index.js
@@ -16,33 +16,57 @@
    isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
+
 function isAllTrue(array, fn) {
+    let isArr = Array.isArray(array);
+    let result = true;
 
-    // var arr = [];
+    if (array.length == 0 || !isArr) {
+        throw new Error('empty array');
+    }
 
-    // for (var i = 0; i < array.length; i++) {
-    //     arr[i] = fn(array[i], i, array);
-    // }
+    for (let i = 0; i < array.length; i++) {
+        if (fn(array[i]) === false) {
+            result = false;
+        }
+    }
 
+    return result;
 }
-
 /*
- Задание 2:
+  Задание 2:
 
- 2.1: Функция принимает массив и фильтрующую фукнцию и должна вернуть true или false
- Функция должна вернуть true если fn вернула true хотя бы для одного из элементов массива
+  2.1: Функция принимает массив и фильтрующую фукнцию и должна вернуть true или false
+  Функция должна вернуть true если fn вернула true хотя бы для одного из элементов массива
 
- 2.2: Необходимо выбрасывать исключение в случаях:
-   - array не массив или пустой массив (с текстом "empty array")
-   - fn не является функцией (с текстом "fn is not a function")
+  2.2: Необходимо выбрасывать исключение в случаях:
+    - array не массив или пустой массив (с текстом "empty array")
+    - fn не является функцией (с текстом "fn is not a function")
 
- Зарпещено использовать встроенные методы для работы с массивами
+  Зарпещено использовать встроенные методы для работы с массивами
 
- Пример:
-   isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true
-   isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
- */
-function isSomeTrue(array, fn) {}
+  Пример:
+    isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true
+    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
+  */
+function isSomeTrue(array, fn) {
+    var isArr = Array.isArray(array);
+
+    if (array.length == 0 || !isArr) {
+        throw new Error('empty array');
+    }
+    let result = true;
+
+    for (let i = 0; i < array.length; i++) {
+        if (fn(array[i]) == true) {
+            result = true;
+        } else {
+            result = false;
+        }
+    }
+
+    return result;
+}
 
 /*
  Задание 3:
@@ -55,8 +79,23 @@ function isSomeTrue(array, fn) {}
  3.3: Необходимо выбрасывать исключение в случаях:
    - fn не является функцией (с текстом "fn is not a function")
  */
-function returnBadArguments(fn) {}
+function returnBadArguments(fn) {
+    var arr = [];
 
+    if (typeof fn != 'function') {
+        throw new Error('fn is not a function');
+    }
+
+    for (var i = 1; i < arguments.length; i++) {
+        try {
+            fn(arguments[i]);
+        } catch (e) {
+            arr.push(arguments[i]);
+        }
+    }
+
+    return arr;
+}
 /*
  Задание 4:
 
@@ -74,7 +113,63 @@ function returnBadArguments(fn) {}
    - number не является числом (с текстом "number is not a number")
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator() {}
+function calculator(number = 0) {
+
+    if (typeof number !== 'number') {
+        throw new Error('number is not a number')
+    }
+
+    function sum() {
+        var result = number
+
+        for (var i = 0; i < arguments.length; i++) {
+            result = result + arguments[i];
+        }
+
+        return result
+    }
+
+    function dif() {
+        var result = number
+
+        for (var i = 0; i < arguments.length; i++) {
+            result = result - arguments[i]
+        }
+
+        return result
+    }
+
+    function div() {
+        var result = number
+
+        for (var i = 0; i < arguments.length; i++) {
+            result = result / arguments[i]
+            if (arguments[i] === 0) {
+                throw new Error('division by 0')
+            }
+        }
+
+        return result
+    }
+
+    function mul() {
+        var result = number
+
+        for (var i = 0; i < arguments.length; i++) {
+            result = result * arguments[i]
+        }
+
+        return result
+    }
+
+    return {
+        sum: sum,
+        dif: dif,
+        div: div,
+        mul: mul
+    }
+
+}
 
 /* При решении задач, пострайтесь использовать отладчик */
 
